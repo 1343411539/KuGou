0d36b0175079a94a2f11a128cfa07b9e68e822b5
243127624y <243127624yulong>
2019-11-20 00:11:06 +0800

    禹清龙

--- app/src/main/java/android/hhh/com/kugou/MainActivity.java
+++ app/src/main/java/android/hhh/com/kugou/MainActivity.java
@@ -1,5 +1,6 @@
 package android.hhh.com.kugou;
 
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -8,50 +9,111 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private LinearLayout menuLayout;
-    private LinearLayout searchLayout;
+    private FrameLayout searchLayout;
     private List<Fragment> fragmentList;
+    private ImageView serachImage;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //动态设置 menuLayout和searchLayout 的大小
-        DisplayMetrics dm=getResources().getDisplayMetrics();
-        int screenWidth=dm.widthPixels;
-        int screenHeight=dm.heightPixels;
-        menuLayout=(LinearLayout)findViewById(R.id.menu_layout);
-        searchLayout=(LinearLayout)findViewById(R.id.search_layout);
-        LinearLayout.LayoutParams params=new LinearLayout.LayoutParams(screenWidth,screenHeight/12);
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        int screenWidth = dm.widthPixels;
+        int screenHeight = dm.heightPixels;
+        menuLayout = (LinearLayout) findViewById(R.id.menu_layout);
+        searchLayout = (FrameLayout) findViewById(R.id.search_layout);
+        serachImage = (ImageView) findViewById(R.id.search_image);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(screenWidth, screenHeight / 12);
         menuLayout.setLayoutParams(params);
         searchLayout.setLayoutParams(params);
         //构造适配器
-        List<Fragment> fragments= new ArrayList<Fragment>();
+        List<Fragment> fragments = new ArrayList<Fragment>();
         fragments.add(new me_Fragment());
         fragments.add(new listen_Fragment());
         fragments.add(new look_Fragment());
-        FragAdapter adapter=new FragAdapter(getSupportFragmentManager(),fragments);
-        ViewPager vp=(ViewPager)findViewById(R.id.viewpager);
+        FragAdapter adapter = new FragAdapter(getSupportFragmentManager(), fragments);
+        ViewPager vp = (ViewPager) findViewById(R.id.viewpager);
         vp.setAdapter(adapter);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+    public void search_click(View view) {
+        serachImage.setVisibility(View.INVISIBLE);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
     public class FragAdapter extends FragmentPagerAdapter {
-        public FragAdapter(FragmentManager fm, List<Fragment> fragments){
+        public FragAdapter(FragmentManager fm, List<Fragment> fragments) {
             super(fm);
-            fragmentList=fragments;
+            fragmentList = fragments;
         }
+
         @Override
-        public Fragment getItem(int arg0){
+        public Fragment getItem(int arg0) {
             return fragmentList.get(arg0);
         }
+
         @Override
-        public int getCount(){
+        public int getCount() {
             return fragmentList.size();
         }
     }
