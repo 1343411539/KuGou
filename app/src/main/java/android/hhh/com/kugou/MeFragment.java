aa9bccfe165713f9313065a36a09db9bc7e1ef05
xiong0508 <1428693744@qq.com>
2019-11-26 22:45:47 +0800

    熊力

--- app/src/main/java/android/hhh/com/kugou/MeFragment.java
+++ app/src/main/java/android/hhh/com/kugou/MeFragment.java
@@ -7,7 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import static android.R.id.list;
 
 /**
  * Created by Administrator on 2019/11/17 0017.
@@ -15,8 +18,10 @@ import android.widget.LinearLayout;
 public class MeFragment extends Fragment implements View.OnClickListener {
 
     private LinearLayout head, local, collect, download, recently,
-            musicHub, kSong, songMenu, generalize;
+            musicHub, kSong, songMenu, menuList, generalize;
     private View button;
+    private ImageView upDown;
+    private boolean isHide = true;// 自建歌单 列表 是否是隐藏的，默认为true
 
     @Nullable
     @Override
@@ -35,6 +40,13 @@ public class MeFragment extends Fragment implements View.OnClickListener {
         setOnclick();
     }
 
+    /**
+     * VISIBLE:0  意思是可见的
+     * INVISIBILITY:4 意思是不可见的，但还占着原来的空间
+     * GONE:8  意思是不可见的，不占用原来的布局空间
+     *
+     * @param v
+     */
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -53,6 +65,15 @@ public class MeFragment extends Fragment implements View.OnClickListener {
             case R.id.ll_k_song:
                 break;
             case R.id.ll_create_song_menu:
+                if (isHide == true) {
+                    upDown.setImageResource(R.drawable.ic_up_x);
+                    menuList.setVisibility(v.VISIBLE);
+                    isHide = false;
+                } else {
+                    upDown.setImageResource(R.drawable.ic_down_x);
+                    menuList.setVisibility(v.GONE);
+                    isHide = true;
+                }
                 break;
             case R.id.ll_generalize:
                 break;
@@ -73,11 +94,13 @@ public class MeFragment extends Fragment implements View.OnClickListener {
         musicHub.setOnClickListener(this);
         kSong.setOnClickListener(this);
         songMenu.setOnClickListener(this);
+        menuList.setOnClickListener(this);
         generalize.setOnClickListener(this);
         button.setOnClickListener(this);
     }
 
     private void initView() {
+        //初始化LineaLayout控件
         head = getActivity().findViewById(R.id.ll_head);
         local = getActivity().findViewById(R.id.ll_local);
         collect = getActivity().findViewById(R.id.ll_collect);
@@ -86,7 +109,13 @@ public class MeFragment extends Fragment implements View.OnClickListener {
         musicHub = getActivity().findViewById(R.id.ll_music_hub);
         kSong = getActivity().findViewById(R.id.ll_k_song);
         songMenu = getActivity().findViewById(R.id.ll_create_song_menu);
+        menuList = getActivity().findViewById(R.id.ll_list);
         generalize = getActivity().findViewById(R.id.ll_generalize);
+
+        //初始化ImageView控件
+        upDown = getActivity().findViewById(R.id.iv_down_up);
+
+        //初始化Button控件
         button = getActivity().findViewById(R.id.btn_user_defined);
     }
 }
