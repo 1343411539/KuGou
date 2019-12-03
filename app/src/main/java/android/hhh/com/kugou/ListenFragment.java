547f4540dfd33b1dd47304b7843852e092c7f5f5
Administrator <ws7777878>
2019-12-03 16:26:44 +0800

    王淞

--- app/src/main/java/android/hhh/com/kugou/ListenFragment.java
+++ app/src/main/java/android/hhh/com/kugou/ListenFragment.java
@@ -1,211 +1,110 @@
 package android.hhh.com.kugou;
 
+import android.content.Context;
 import android.content.Intent;
 import android.hhh.com.kugou.wangsong.Love;
 import android.hhh.com.kugou.wangsong.MusicLibrary;
 import android.hhh.com.kugou.wangsong.SongSheet;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.annotation.Nullable;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
+
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+import com.youth.banner.listener.OnBannerListener;
+import com.youth.banner.loader.ImageLoader;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+
 
 /**
  * Created by Administrator on 2019/11/17 0017.
  */
 public class ListenFragment extends android.support.v4.app.Fragment {
     private View mView;
-    private ViewPager mViewPaper;
-    private List<ImageView> images;
-    private List<View> dots;
-    private int currentItem;
-    //记录上一次点的位置
-    private int oldPosition = 0;
-    //存放图片的id
-    private int[] imageIds = new int[]{
-            R.drawable.background1,
-            R.drawable.background2,
-            R.drawable.background3,
-            R.drawable.background4,
-            R.drawable.background5
-    };
-    //存放图片的标题
-    private String[] titles = new String[]{
-            "轮播1",
-            "轮播2",
-            "轮播3",
-            "轮播4",
-            "轮播5"
-    };
-    private TextView title;
-    private ViewPagerAdapter adapter;
-    private ScheduledExecutorService scheduledExecutorService;
-
+    private Banner banner;
     private ImageButton musiclibrary_ib, songsheet_ib, love_ib;
 
+    public ListenFragment() {
+        // Required empty public constructor
+    }
 
-    @Nullable
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.fragment_listen, null);
         setView();//轮转播放图片添加
 
-        initView(); //按钮触发事件
+        initView();//按钮点击事件
 
         return mView;
     }
 
+    public void setView() {
+        banner = mView.findViewById(R.id.banner);
 
-    //轮转播放图片添加
-    private void setView() {
-        mViewPaper = (ViewPager) mView.findViewById(R.id.vp);
+        ArrayList<Integer> imgs = new ArrayList<>();
+        imgs.add(R.drawable.background1);
+        imgs.add(R.drawable.background2);
+        imgs.add(R.drawable.background3);
+        imgs.add(R.drawable.background4);
+        imgs.add(R.drawable.background5);
 
-        //显示的图片
-        images = new ArrayList<ImageView>();
-        for (int i = 0; i < imageIds.length; i++) {
-            ImageView imageView = new ImageView(getActivity());
-            imageView.setBackgroundResource(imageIds[i]);
-            images.add(imageView);
-        }
-        //显示的小点
-        dots = new ArrayList<View>();
-        dots.add(mView.findViewById(R.id.dot_0));
-        dots.add(mView.findViewById(R.id.dot_1));
-        dots.add(mView.findViewById(R.id.dot_2));
-        dots.add(mView.findViewById(R.id.dot_3));
-        dots.add(mView.findViewById(R.id.dot_4));
+        ArrayList<String> title = new ArrayList<>();
+        title.add("图1");
+        title.add("图2");
+        title.add("图3");
+        title.add("图4");
+        title.add("图5");
 
-        title = (TextView) mView.findViewById(R.id.title);
-        title.setText(titles[0]);
-
-        adapter = new ViewPagerAdapter();
-        mViewPaper.setAdapter(adapter);
-
-        mViewPaper.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
-
+        //设置内置样式.
+        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE_INSIDE);
+        //设置图片加载器，图片加载器在下方
+        banner.setImageLoader(new MyImageLoader());
+        //设置图片的集合
+        banner.setImages(imgs);
+        //设置轮播的动画效果，内含多种特效，可点入方法内查找后内逐一体验
+        banner.setBannerAnimation(Transformer.Default);
+        //设置轮播图的标题集合
+        banner.setBannerTitles(title);
+        //设置轮播间隔时间
+        banner.setDelayTime(5000);
+        //设置是否为自动轮播，默认是“是”。
+        banner.isAutoPlay(true);
+        //设置指示器的位置。
+        banner.setIndicatorGravity(BannerConfig.CENTER);
+        //以上内容都可写成链式布局，这是轮播图的监听。比较重要。方法在下面。
+        banner.setOnBannerListener(new OnBannerListener() {
             @Override
-            public void onPageSelected(int position) {
-                title.setText(titles[position]);
-                dots.get(position).setBackgroundResource(R.drawable.indicator_select);
-                dots.get(oldPosition).setBackgroundResource(R.drawable.indicator_no_select);
-
-                oldPosition = position;
-                currentItem = position;
-            }
-
-            @Override
-            public void onPageScrolled(int arg0, float arg1, int arg2) {
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int arg0) {
-
+            public void OnBannerClick(int position) {
+                Log.i("tag", "你点了第"+position+"张轮播图");
             }
         });
-    }
 
-    /*定义的适配器*/
-    public class ViewPagerAdapter extends PagerAdapter {
-
-        @Override
-        public int getCount() {
-            return images.size();
-        }
-
-        @Override
-        public boolean isViewFromObject(View arg0, Object arg1) {
-            return arg0 == arg1;
-        }
-
-        @Override
-        public void destroyItem(ViewGroup view, int position, Object object) {
-            // TODO Auto-generated method stub
-//          super.destroyItem(container, position, object);
-//          view.removeView(view.getChildAt(position));
-//          view.removeViewAt(position);
-            view.removeView(images.get(position));
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup view, int position) {
-            // TODO Auto-generated method stub
-            view.addView(images.get(position));
-            return images.get(position);
-        }
-
-    }
-
-    /**
-     * 利用线程池定时执行动画轮播
-     */
-    @Override
-    public void onStart() {
-        // TODO Auto-generated method stub
-        super.onStart();
-        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-        scheduledExecutorService.scheduleWithFixedDelay(
-                new ViewPageTask(),
-                7,
-                7,
-                TimeUnit.SECONDS);
+        //必须最后调用的方法，启动轮播图。
+        banner .start();
     }
 
 
-    /**
-     * 图片轮播任务
-     *
-     * @author liuyazhuang
-     */
-    private class ViewPageTask implements Runnable {
-
+    private class MyImageLoader extends ImageLoader {
         @Override
-        public void run() {
-            currentItem = (currentItem + 1) % imageIds.length;
-            mHandler.sendEmptyMessage(0);
-        }
-    }
-
-    /**
-     * 接收子线程传递过来的数据
-     */
-    private Handler mHandler = new Handler() {
-        public void handleMessage(android.os.Message msg) {
-            mViewPaper.setCurrentItem(currentItem);
-        }
-
-        ;
-    };
-
-    @Override
-    public void onStop() {
-        // TODO Auto-generated method stub
-        super.onStop();
-        if (scheduledExecutorService != null) {
-            scheduledExecutorService.shutdown();
-            scheduledExecutorService = null;
+        public void displayImage(Context context, Object path, ImageView imageView) {
+            imageView.setImageResource(Integer.parseInt(path.toString()));
         }
     }
 
 
     private void initView() {
-        musiclibrary_ib = (ImageButton) mView.findViewById(R.id.musiclibrary_ib);
+        musiclibrary_ib = mView.findViewById(R.id.musiclibrary_ib);
         musiclibrary_ib.setOnClickListener(new ButtonListener());
-        songsheet_ib = (ImageButton) mView.findViewById(R.id.songsheet_ib);
+        songsheet_ib = mView.findViewById(R.id.songsheet_ib);
         songsheet_ib.setOnClickListener(new ButtonListener());
         love_ib=mView.findViewById(R.id.love_ib);
         love_ib.setOnClickListener(new ButtonListener());
